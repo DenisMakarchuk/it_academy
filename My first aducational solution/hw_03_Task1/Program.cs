e5f14da6bb61279c23936861867c52774ab4eab2
Denis Makarchuk <hiedmant@gmail.com>
2019-07-15 13:50:10 +0300

    [+] hw_03_Task1

---
+++ My first aducational solution/hw_03_Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_03_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Plus();
+                Console.Read();
+            }
+            void Plus()
+            {
+                double x, y, z;
+                Console.WriteLine("Введите первое слагаемое");
+                x = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите второе слагаемое");
+                y = Convert.ToDouble(Console.ReadLine());
+                z = x + y;
+                Console.WriteLine($"Сумма {x} и {y} равна {z}");
+            }
+        }
+    }
+}
