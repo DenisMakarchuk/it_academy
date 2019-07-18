238e88b29063464ffd64d4ff139cf4fb3a334208
Denis Makarchuk <hiedmant@gmail.com>
2019-07-18 18:05:42 +0300

    [+] hw_08_Task1

---
+++ My first aducational solution/hw_08_Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_08_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text, separate the lines with \";\"");
+            string poem = Console.ReadLine();
+            string[] poemArr = poem.Split(';');
+
+            for (int i = 0; i < poemArr.Length; i++)
+            {
+                poemArr[i] = poemArr[i].Replace('o', 'a').Replace('O','A');
+                //    for (int j = 0; j < poemArr[i].Length; j++)
+                //    {
+
+                //        switch (poemArr[i][j])
+                //        {
+                //            case 'o':
+                //                poemArr[i][j] = 'a';
+                //                break;
+                //            case 'O':
+                //                poemArr[i][j] = 'A';
+                //                break;
+                //        }
+
+                //    }
+            }
+            for (int i = 0; i < poemArr.Length; i++)
+            {
+                Console.WriteLine(poemArr[i]);
+            }
+
+            Console.Read();
+        }
+    }
+}
