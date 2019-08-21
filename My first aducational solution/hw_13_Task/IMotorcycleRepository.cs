36078543dde5721210d4fa994d6a4791687b3838
Denis Makarchuk <hiedmant@gmail.com>
2019-08-21 12:45:46 +0300

    [+] hw_13_Task

---
+++ My first aducational solution/hw_13_Task/IMotorcycleRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_13_Task
+{
+    interface IMotorcycleRepository
+    {
+        void CreateMotorcycle(Motorcycle moto);
+
+        Motorcycle GetMotorcycleByID(int id);
+
+        List<Motorcycle> GetMotorcycles();
+
+        Motorcycle UpdateMotorcycle(Motorcycle moto);
+
+        void DeleteMotorcycle(int id);
+    }
+}
---
+++ My first aducational solution/hw_13_Task/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_13_Task
+{
+    class Program
+    {
+        public static readonly ILog log = LogManager.GetLogger(typeof(Program));
+        static void Main(string[] args)
+        {
+            MotorcycleRepository motorcycleRepository = new MotorcycleRepository();
+
+            Motorcycle motorcycle = new Motorcycle("Honda", "CB", 2014, 23);
+
+            Logger.Log.Info("CRUD operations started!");
+            motorcycleRepository.CreateMotorcycle(motorcycle);
+
+            Motorcycle moto = motorcycleRepository.GetMotorcycleByID(1);
+
+            Logger.Log.Info("Start of console output");
+            Console.WriteLine($"The name of the motorcycle: {moto.Name}\n" +
+                $"The model of motorcycle: {moto.Model}\n" +
+                $"The year of issure: {moto.Year}\n" +
+                $"The odometer of the motorcycle: {moto.Odometer}");
+
+            Console.Read();
+            Logger.Log.Info("Program completed successfully");
+        }
+    }
+}
